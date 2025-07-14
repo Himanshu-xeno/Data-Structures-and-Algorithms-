0e269661cc3d9bad4b64b0be244fd18f43545c14
Himanshu Tiwari <himanshusktiwari@gmail.com>
2025-07-14 20:08:35 +0530

    Added the Count good numbers

--- A2Z_sheet/Recursion/Recursion(Pattern-wise)/Recursion(Get a hold)/GoodNumbers.java
+++ A2Z_sheet/Recursion/Recursion(Pattern-wise)/Recursion(Get a hold)/GoodNumbers.java
@@ -4,6 +4,40 @@
 
 public class GoodNumbers {
     public static void main(String[] args) {
-        
+
+        System.out.println("n = 1 : " + countGoodNumbers(1));  //output:5
+        System.out.println("n = 50 : " + countGoodNumbers(50)); //output : 564908303
+    }
+
+
+
+    //Function to calculate (base^exponent)%MOD
+    static long power(long base, long exponent, int mod){
+        long results = 1;
+        while(exponent > 0){
+            if(exponent % 2 == 1){   // if expo is odd
+                results = (results * base) % mod;
+            }
+            base = (base * base) % mod;   // square the base
+            exponent = exponent / 2;  // divide expo by 2
+        }
+        return results;
+    }
+
+
+
+    //Function to count good digit strings
+    static int countGoodNumbers(long n){
+        int MOD = 1000000007;
+
+        long evenPositions = (n+1)/2;  //count of positions
+        long oddPositions = (n/2);   //count of positions
+
+        long evenWays = power(5,evenPositions,MOD);  //5^evenPositions % MOD
+        long oddWays = power(4,oddPositions,MOD);    //4^evenPositions % MOD
+
+        return (int) ((evenWays * oddWays) % MOD);  //Total ways
+
+
     }
 }
