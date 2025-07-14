5d3118a688983e4614b81c3d2bb217cafcbae6c3
Himanshu Tiwari <himanshusktiwari@gmail.com>
2025-07-14 19:25:18 +0530

    Structured the Recursion folder

---
+++ A2Z_sheet/Recursion/Recursion(Pattern-wise)/Recursion(Get a hold)/GoodNumbers.java
@@ -0,0 +1,9 @@
+//(1922) A digit string is considered good if the digits at even indices (0-based) are even digits (0, 2, 4, 6, 8) and the digits at odd indices are prime digits (2, 3, 5, 7).
+//Given an integer n, return the total number of good digit strings of length n. As the result may be large, return it modulo 109 + 7.
+//A digit string is a string consisting only of the digits '0' through '9'. It may contain leading zeros.
+
+public class GoodNumbers {
+    public static void main(String[] args) {
+        
+    }
+}
