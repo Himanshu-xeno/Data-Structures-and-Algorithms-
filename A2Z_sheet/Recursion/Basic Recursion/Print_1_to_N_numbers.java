d16fe28e4a721a4d9ee05c01524f3bc7a7a80203
Himanshu Tiwari <himanshusktiwari@gmail.com>
2025-07-17 23:18:17 +0530

    Edited Basic recursion

--- A2Z_sheet/Recursion/Basic Recursion/Print_1_to_N_numbers.java
+++ A2Z_sheet/Recursion/Basic Recursion/Print_1_to_N_numbers.java
@@ -1,10 +1,11 @@
 public class Print_1_to_N_numbers {
     public static void main(String[] args) {
-        printNum(5);
+        // printNum(5);
+        printNum(10);
     }
 
-    static void printNum(int n){
-        if(n == 0){
+    static void printNum(int n) {
+        if (n == 0) {
             return;
         }
         printNum(n - 1);
