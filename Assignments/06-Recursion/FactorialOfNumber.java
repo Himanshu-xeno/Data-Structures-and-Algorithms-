ad3d77eeabcdfbf909586a6587a0184913e3205a
Himanshu Tiwari <himanshusktiwari@gmail.com>
2025-07-24 14:11:54 +0530

    Added the iterative 2 codes for factorial

--- Assignments/06-Recursion/FactorialOfNumber.java
+++ Assignments/06-Recursion/FactorialOfNumber.java
@@ -4,17 +4,38 @@
 
 public class FactorialOfNumber {
     public static void main(String[] args) {
-        int ans = fact(5);
+        int ans = factoFor(5);
+        //int ans = factoWhile(5);
+        //int ans = fact(5);
         System.out.println(ans);
     }
 
     //Recursive approach for factorial   tc=O(n), sc=O(n) -> call stack
     static int fact(int n){
 
-        if(n <= 1) return 1; //base condition
+        if(n <= 1) return 1; //base condition    //Donot forget to keep <= sign
 
         return n * fact(n-1);
     }
 
+    //iterative code - while loop
+    static int factoWhile(int n){
+        int fact = 1;
+        int i = 2;
+        while(i <= n){          //Donot forget to keep <= sign
+            fact *= i;
+            i++;
+        }
+        return fact;
+    }
+
+    //iterative code - for loop
+    static int factoFor(int n){
+        int fact = 1;
+        for (int i = 2; i <= n; i++) {   //Donot forget to keep <= sign
+            fact *= i;
+        }
+        return fact;
+    }
 
 }
