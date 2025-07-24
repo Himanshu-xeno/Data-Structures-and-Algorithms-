3933e5e4fca9ae90a124f1d150b184418bd352e7
Himanshu Tiwari <himanshusktiwari@gmail.com>
2025-07-24 14:41:01 +0530

    Added the Arrayis sorted code

--- Assignments/06-Recursion/CheckArraySorted.java
+++ Assignments/06-Recursion/CheckArraySorted.java
@@ -4,9 +4,12 @@
 public class CheckArraySorted {
     public static void main(String[] args) {
         int[] arr = {1,2,5,4,4,5,6};
-        System.out.println(isSorted(arr));
-    }
+        System.out.println(isSorted(arr));           // nested loop version
+        System.out.println(isSortedOneLoop(arr));    // one-loop version
+        System.out.println(isSortedRecursive(arr, 0)); // recursive version
+        }
 
+    //iterative : using 2 pointers    tc=O(n^2), sc=O(1)
     static boolean isSorted(int[] arr){
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j < arr.length - 1 ; j++) {
@@ -14,5 +17,25 @@ public class CheckArraySorted {
             }
         }
         return true;
+    }
+
+    //iterative : using one loop itself    tc=O(n), sc=O(1)
+    static boolean isSortedOneLoop(int[] arr){
+        for (int i = 1; i < arr.length - 1 ; i++) {
+            if(arr[i] < arr[i-1]) return false;
+        }
+        return true;
+    }
+
+    //Recursive approach        tc=O(n), sc=O(n)
+    static boolean isSortedRecursive(int[] arr, int index) {
+        // Base case: If we reach the last element, it's sorted
+        if (index == arr.length - 1) return true;
+
+        // If current element is greater than the next, it's not sorted
+        if (arr[index] > arr[index + 1]) return false;
+
+        // Recursive call for next pair
+        return isSortedRecursive(arr, index + 1);
     }
 }
