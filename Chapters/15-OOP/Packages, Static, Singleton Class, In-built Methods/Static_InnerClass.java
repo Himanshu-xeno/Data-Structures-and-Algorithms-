9c26002c4050faffd3c259e0f4bc099aa151dc3a
Himanshu Tiwari <himanshusktiwari@gmail.com>
2025-07-25 15:40:57 +0530

    Added the static inner class code 1

---
+++ Chapters/15-OOP/Packages, Static, Singleton Class, In-built Methods/Static_InnerClass.java
@@ -0,0 +1,28 @@
+//Innerrclass outside the main(outer class) {Top-level class}
+class Test {
+    String name;
+
+    public Test(String name) {
+        this.name = name;
+    }
+}
+
+
+
+public class Static_InnerClass {
+//    static class Test{
+//        String name;
+//
+//        public Test(String name) {
+//            this.name = name;
+//        }
+
+
+        public static void main(String[] args) {
+            Test a = new Test("Kunal");
+            Test b = new Test("Rahul");
+
+            System.out.println(a.name);
+            System.out.println(b.name);
+        }
+}
