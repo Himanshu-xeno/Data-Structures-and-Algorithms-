4e0e18db8a4d8598078ed76c0e0039e36e47ae9e
Himanshu Tiwari <himanshusktiwari@gmail.com>
2025-07-25 21:01:20 +0530

    Added the Override code for toString() in static InnerClass

--- Chapters/15-OOP/Packages, Static, Singleton Class, In-built Methods/Static_InnerClass.java
+++ Chapters/15-OOP/Packages, Static, Singleton Class, In-built Methods/Static_InnerClass.java
@@ -1,21 +1,29 @@
 //Innerrclass outside the main(outer class) {Top-level class}
-class Test {
-    String name;
-
-    public Test(String name) {
-        this.name = name;
-    }
-}
+//class Test {
+//    String name;
+//
+//    public Test(String name) {
+//        this.name = name;
+//    }
+//}
 
 
 
 public class Static_InnerClass {
-//    static class Test{
-//        String name;
-//
-//        public Test(String name) {
-//            this.name = name;
-//        }
+    static class Test {
+        String name;
+
+        //used for overriding the toString() method
+        @Override
+        public String toString() {
+            return name;
+        }
+
+        public Test(String name) {
+            this.name = name;
+        }
+    }
+
 
 
         public static void main(String[] args) {
@@ -24,5 +32,7 @@ public class Static_InnerClass {
 
             System.out.println(a.name);
             System.out.println(b.name);
+
+            System.out.println(a);
         }
 }
