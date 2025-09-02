a6e2f56ae0a5afbc558d7c43a03c91e3ba690799
Himanshu Tiwari <himanshusktiwari@gmail.com>
2025-09-02 06:08:09 +0530

    Corrected the code of SelectionSort.java

--- A2Z_sheet/03-Sorting/SelectionSort.java
+++ A2Z_sheet/03-Sorting/SelectionSort.java
@@ -2,34 +2,35 @@ import java.util.Arrays;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] arr = {34,56,34,122};
+        int[] arr = { 34, 56, 34, 122 };
         selection(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    static void selection(int[] arr){
+    static void selection(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            int last = arr.length-i-1;
+            int last = arr.length - i - 1;
 
-            //finding the max element in remaining unsorted elements
-            int maxIndex = getMaxIndex(arr, 0 , last);
+            // finding the max element in remaining unsorted elements
+            int maxIndex = getMaxIndex(arr, 0, last);
             swap(arr, maxIndex, last);
         }
     }
 
-    //Getting the max element index in the unsorted array region - it excludes the last index of array.
-    static int getMaxIndex(int[] arr,int start,int end){
+    // Getting the max element index in the unsorted array region - it excludes the
+    // last index of array.
+    static int getMaxIndex(int[] arr, int start, int end) {
         int max = start;
-        for (int i = 0; i <= end; i++) {
-            if(arr[i] > arr[max]){
+        for (int i = start; i <= end; i++) {
+            if (arr[i] > arr[max]) {
                 max = i;
             }
         }
         return max;
     }
 
-    //swapping the two selected elements
-    static void swap(int[] arr, int first, int second){
+    // swapping the two selected elements
+    static void swap(int[] arr, int first, int second) {
         int temp = arr[first];
         arr[first] = arr[second];
         arr[second] = temp;
