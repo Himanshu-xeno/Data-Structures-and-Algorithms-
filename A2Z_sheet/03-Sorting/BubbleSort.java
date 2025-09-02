c7cff80d3c81ff2c17f4b4464124e865f8fc2e51
Himanshu Tiwari <himanshusktiwari@gmail.com>
2025-09-02 06:23:41 +0530

    Added some comments in BubbleSort.java

--- A2Z_sheet/03-Sorting/BubbleSort.java
+++ A2Z_sheet/03-Sorting/BubbleSort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {34,56,11,34,232,444,2};
+        int[] arr = { 34, 56, 11, 34, 232, 444, 2 };
         bubble(arr, arr.length);
         System.out.println(Arrays.toString(arr));
     }
@@ -11,14 +11,16 @@ public class BubbleSort {
         for (int i = 0; i < n; i++) {
 
             // One pass of bubble sort. After this pass, the largest element is at the end.
+            // arr[j] -> 2nd element
+            // arr[j-1] -> 1st element
             for (int j = 1; j < n; j++) {
-//            if (arr[j] > arr[j - 1]) { // Descending order
-//                int temp = arr[j];
-//                arr[j] = arr[j - 1];
-//                arr[j - 1] = temp;
-//            }
+                // if (arr[j] > arr[j - 1]) { // Descending order (left < right)
+                // int temp = arr[j];
+                // arr[j] = arr[j - 1];
+                // arr[j - 1] = temp;
+                // }
 
-                if (arr[j] < arr[j - 1]) { // ascending order
+                if (arr[j] < arr[j - 1]) { // ascending order (left > right)
                     int temp = arr[j];
                     arr[j] = arr[j - 1];
                     arr[j - 1] = temp;
