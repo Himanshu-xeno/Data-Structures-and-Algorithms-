01a8ec3a631c6c53e55869e43dc18fe08db28b3e
Himanshu Tiwari <himanshusktiwari@gmail.com>
2025-09-02 21:47:34 +0530

    added the basic Binary search code

---
+++ A2Z_sheet/06-Binary Search/BinarySearch_01.java
@@ -0,0 +1,27 @@
+public class BinarySearch_01 {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,45,55,66,666,4555};
+        int target = 4555;
+        int ans = searc(arr, target);
+        System.out.println("The index of target element is : " + ans);
+    }
+
+    static int searc(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;  //because we are considering the index of array
+
+        while (start <= end){
+            int mid = start + (end - start)/2;
+
+            if(target > arr[mid]){
+                start = mid + 1;  //search in right side
+            }else if(target < arr[mid]){
+                end = mid - 1;  //search in left side
+            }else{
+                return mid;  //found the target
+            }
+        }
+        return -1;
+    }
+
+}
