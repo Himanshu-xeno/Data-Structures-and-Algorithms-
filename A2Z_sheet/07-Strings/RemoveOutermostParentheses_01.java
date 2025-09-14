bb4a68e97c3600c2c8ce531d2861b077595ff96d
Himanshu Tiwari <himanshusktiwari@gmail.com>
2025-09-14 22:14:53 +0530

    Added the code for Remove outer paranthesis in Substrings folder

---
+++ A2Z_sheet/07-Strings/RemoveOutermostParentheses_01.java
@@ -0,0 +1,46 @@
+public class RemoveOutermostParentheses_01 {
+    public static void main(String[] args) {
+        String s1 = "(()())(())()";
+        System.out.println(removeOuterParentheses(s1));
+        System.out.println(removeOuter(s1));
+    }
+
+    static String removeOuterParentheses(String str){
+        int openCount = 0;
+        int closeCount = 0;
+        String result = "";
+        int start = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if(c == '('){
+                openCount ++;
+            } else{
+                closeCount++;
+            }
+
+            if(openCount == closeCount){
+                result = result + str.substring(start + 1, i);
+                start = i+1;
+            }
+        }
+        return result;
+    }
+
+    //optimal method -> using StringBuilder
+    static String removeOuter(String str) {
+        int openCount = 0, closeCount = 0, start = 0;
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == '(') openCount++;
+            else closeCount++;
+            if (openCount == closeCount) {
+                result.append(str.substring(start + 1, i));
+                start = i + 1;
+            }
+        }
+        return result.toString();
+    }
+}
