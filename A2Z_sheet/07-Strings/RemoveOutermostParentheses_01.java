806a5f72a5098b3cd41f723fabcd6901713d2f6b
Himanshu Tiwari <himanshusktiwari@gmail.com>
2025-09-16 06:15:56 +0530

    Added the most  optimal code to RemoveOuterParanthesis

--- A2Z_sheet/07-Strings/RemoveOutermostParentheses_01.java
+++ A2Z_sheet/07-Strings/RemoveOutermostParentheses_01.java
@@ -3,8 +3,26 @@ public class RemoveOutermostParentheses_01 {
         String s1 = "(()())(())()";
         System.out.println(removeOuterParentheses(s1));
         System.out.println(removeOuter(s1));
+        System.out.println(removing(s1));
     }
 
+    //Most optimal way
+    static String removing(String str){
+        int balance = 0;
+
+        StringBuilder sb = new StringBuilder(str.length());
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if(c == ')') balance --;
+            if(balance != 0) sb.append(c);
+            if(c == '(') balance ++;
+        }
+        return sb.toString();
+    }
+
+    //Most baisc method - brute force
     static String removeOuterParentheses(String str){
         int openCount = 0;
         int closeCount = 0;
