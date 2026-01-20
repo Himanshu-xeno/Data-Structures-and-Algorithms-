4d286dbca6a7cc27d458b92ab4d698b683b82791
Himanshu Tiwari <himanshusktiwari@gmail.com>
2026-01-20 19:48:54 +0530

    Added the linkedlist : Insertion , Deletion

--- Chapters/16-LinkedList/LL.java
+++ Chapters/16-LinkedList/LL.java
@@ -1,12 +1,108 @@
 public class LL {
 
         private Node head;
-        private Node next;
+        private Node tail;
 
         private int size;
 
         public LL() {
             this.size = 0;
+        }
+
+        //Insetion at first code
+        public void insertFirst(int val){
+            Node node = new Node(val);  //create a new node(box)
+            node.next = head;   //Point the head of new node to current head
+            head = node;
+
+            if(tail == null){
+                tail = head;
+            }
+
+            size += 1;
+        }
+
+        //Insertion at last
+        public void insertLast(int val){
+            if(tail == null){
+                insertFirst(val);
+                return;
+            }
+            Node node = new Node(val);
+            tail.next = node;
+            tail = node;
+            size ++;
+        }
+
+        //Insert at any index
+        //head->10->20->30->40->50->null
+    public void insert(int val, int index){
+            if(index == 0){
+                insertFirst(val);
+                return;
+            }
+            if(index == size){
+                insertLast(val);
+                return;
+            }
+            //To insert at 2, stand at 1
+            //use temp to move as we should not move head
+            Node temp = head;   //head = temp = 10 : move forward
+            for (int i = 1; i < index; i++) {
+                temp = temp.next;   //stop at (i-1) : 1(20)
+                                    //20->30 {temp.next=30}(this is basic understanding)
+            }
+            Node node = new Node(val, temp.next);   //Use of the constructor below
+            //node = 99 -> 30
+            temp.next = node;   //Change 20.next = 99 -> 30
+
+            size ++;
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    //Delete at First
+    public int deleteFirst(){
+            int val = head.value;
+            head = head.next;
+            if(head == null){
+                tail = null;
+            }
+            size --;
+            return val;
+    }
+
+    //Delete Last Element
+    public Node get(int index){
+            Node node = head;
+            for(int i=0; i< index; i++){
+                node = node.next;
+            }
+            return node;
+    }
+
+    public int deleteLast(){
+            if(size <= 1){
+                return deleteFirst();
+            }
+            Node secondLast = get(size-2);
+            int val = tail.value;
+            tail = secondLast;
+            tail.next = null;
+            size --;
+
+            return val;
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
+
+    public void display(){
+            Node temp = head;   //Pointing to head
+            while(temp != null){
+                System.out.print (temp.value + " -> ");
+                temp = temp.next; //Move ahead
+            }
+            System.out.println("END");
         }
 
     private class Node{
--- Chapters/16-LinkedList/Main.java
+++ Chapters/16-LinkedList/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list.insertFirst(5);
+        list.insertFirst(6);
+        list.insertFirst(7);
+        list.insertFirst(8);
+
+        list.insertLast(99);
+
+        System.out.println(list.deleteFirst());
+        list.insertFirst(8);
+        list.insert(102,3);
+        list.display();
 
     }
 }
